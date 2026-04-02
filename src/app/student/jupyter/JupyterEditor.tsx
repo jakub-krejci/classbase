df38d86c8b9c157a1808ca773c5a741259ca3b5d
jakub-krejci <krejci.jakub12@gmail.com>
2026-04-02 11:41:39 +0200

    Update JupyterEditor.tsx

--- src/app/student/jupyter/JupyterEditor.tsx
+++ src/app/student/jupyter/JupyterEditor.tsx
@@ -525,7 +525,7 @@ export default function JupyterEditor({ profile }: { profile: any }) {
 
     // Before running: write all folder files to Pyodide's virtual filesystem
     // so that pd.read_csv('data/file.csv') works as expected
-    const activeProj = activeProjectRef.current
+    const activeProj = activeProject
     if (activeProj?.folders.length) {
       const { loadPyodide: getPy } = await import('@/lib/pyodide-runner')
       try {
